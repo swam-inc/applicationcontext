bb6fac0ce97383ce6f629b03d3e742a017694894
steven <steven@swaminc.com>
2021-07-26 23:24:49 -0400

    fixed the primitive value null issue

--- src/main/java/com/example/applicationcontext/ApplicationContextApplication.java
+++ src/main/java/com/example/applicationcontext/ApplicationContextApplication.java
@@ -5,19 +5,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.w3c.dom.ls.LSOutput;
 
-@SpringBootApplication
+//@SpringBootApplication
 @Configuration
 @PropertySource("classpath:static/resource.properties")
 
 public class ApplicationContextApplication {
     @Autowired
     Environment environment;
-
+    @Autowired
     @Value("${db.var1}")
     private String var1;
     @Value("${db.var2}")
@@ -27,21 +28,24 @@ public class ApplicationContextApplication {
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
-
+    @Bean
     public void test(){
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextApplication.class);
-        Environment env = ctx.getBean(Environment.class);
-        System.out.println(env.toString());
+        //ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextApplication.class);
+        //Environment env = ctx.getBean(Environment.class);
+        System.out.println("!".repeat(100));
+        //System.out.println(env.toString());
         System.out.println(var1);
         System.out.println(var2);
-        System.out.println(env.getProperty("db.var1"));
-        System.out.println(env.getProperty("db.var2"));
+        //System.out.println(env.getProperty("db.var1"));
+        //System.out.println(env.getProperty("db.var2"));
+        System.out.println("!".repeat(100));
     }
     public static void main(String[] args) {
-        //SpringApplication.run(ApplicationContextApplication.class, args);
-        ApplicationContextApplication application = new ApplicationContextApplication();
+        SpringApplication.run(ApplicationContextApplication.class, args);
+        ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextApplication.class);
+        ApplicationContextApplication application = ctx.getBean(ApplicationContextApplication.class);
         application.test();
-
+        ctx.registerShutdownHook();
     }
 
 }
